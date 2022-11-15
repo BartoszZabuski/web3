704e2d426c351384763d7225d48d2ce91967e69d
bartoszz <bartosz.zabuski@streamamg.com>
2022-11-15 18:19:06 +0100

    feat: on chain battles

--- www.web3.university/nftWithOnChainMeta/contracts/ChainBattles.sol
+++ www.web3.university/nftWithOnChainMeta/contracts/ChainBattles.sol
@@ -13,17 +13,40 @@ contract ChainBattles is ERC721URIStorage {
 
 	Counters.Counter private _tokenIds; 
 
-	mapping(uint256 => uint256) public tokenIdToLevels;
+	// 
+	uint randNonce = 0;
+
+	function randomNumber() private returns (uint){
+		uint random = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % 100;
+		randNonce++;
+		return random;
+	}
+
+	struct CharacterProfile {
+		uint256 strength;
+		uint256 level;
+		uint256 speed;
+	}
+
+	// change mapping to uint256 to struct
+	// struct needs more attributes: levels, strength, hp, speed, whatever?
+	// when minting occurs -> generate pseudo random stat values (oficially you can't do proper random generation in solidity, so go with pseudo random)
+	//  increment each of the stats as you train
+	mapping(uint256 => CharacterProfile) public tokenIdToLevels;
 
 	constructor() ERC721("Chain Battles", "CBTLS"){}
 
 	function generateCharacter(uint256 tokenId) public view returns (string memory) {
+		CharacterProfile memory profile = getProfile(tokenId);
 		bytes memory svg = abi.encodePacked(
-			'<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
-				'<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
-				'<rect width="100%" height="100%" fill="black" />',
-				'<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">Warrior</text>',
-				'<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">Levels: ', getLevels(tokenId), '</text>',
+			'<svg width="800" height="600" xmlns="http://www.w3.org/2000/svg">',
+			'<g>',
+			'<title>Layer 1</title>',
+			'<text transform="matrix(1.65653 0 0 2.14618 -191.706 -194.031)" stroke="#000" xml:space="preserve" text-anchor="start" font-family="Noto Sans JP" font-size="24" id="svg_1" y="191.46594" x="280.53023" stroke-width="0" fill="#000000">Chain warrior</text>',
+			'<text xml:space="preserve" text-anchor="start" font-family="Noto Sans JP" font-size="24" id="svg_2" y="270" x="321" stroke-width="0" stroke="#000" fill="#000000">level:',profile.level.toString(),'</text>',
+			'<text xml:space="preserve" text-anchor="start" font-family="Noto Sans JP" font-size="24" id="svg_3" y="309" x="312" stroke-width="0" stroke="#000" fill="#000000">speed:',profile.speed.toString(),'</text>',
+			'<text xml:space="preserve" text-anchor="start" font-family="Noto Sans JP" font-size="24" id="svg_4" y="346" x="290" stroke-width="0" stroke="#000" fill="#000000">strength:',profile.strength.toString(),'</text>',
+			'</g>',
 			'</svg>'
 		);
 
@@ -35,9 +58,8 @@ contract ChainBattles is ERC721URIStorage {
 		);
 	}
 
-	function getLevels(uint256 tokenId) public view returns (string memory) {
-		uint256 levels = tokenIdToLevels[tokenId];
-		return levels.toString();
+	function getProfile(uint256 tokenId) public view returns (CharacterProfile memory) {
+		return  tokenIdToLevels[tokenId];
 	}
 
 	function getTokenURI(uint256 tokenId) public view returns (string memory){
@@ -60,15 +82,21 @@ contract ChainBattles is ERC721URIStorage {
 		_tokenIds.increment();
 		uint256 newItemId = _tokenIds.current();
 		_safeMint(msg.sender, newItemId);
-		tokenIdToLevels[newItemId] = 0;
+		tokenIdToLevels[newItemId] = CharacterProfile(randomNumber(),randomNumber(),randomNumber());
 		_setTokenURI(newItemId, getTokenURI(newItemId));
 	}
 
 	function train(uint256 tokenId) public {
     	require(_exists(tokenId), "Please use an existing token");
     	require(ownerOf(tokenId) == msg.sender, "You must own this token to train it");
-    	uint256 currentLevel = tokenIdToLevels[tokenId];
-    	tokenIdToLevels[tokenId] = currentLevel + 1;
+    	
+		CharacterProfile memory profile = tokenIdToLevels[tokenId];
+		uint256 currentLevel = profile.level;
+		uint256 currentStrength = profile.strength;
+		uint256 currentSpeed = profile.speed;
+
+    	tokenIdToLevels[tokenId] = CharacterProfile(currentStrength + 1, currentLevel + 1, currentSpeed + 1);
+
     	_setTokenURI(tokenId, getTokenURI(tokenId));
 	}
 
